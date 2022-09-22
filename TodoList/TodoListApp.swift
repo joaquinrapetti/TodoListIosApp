d16a669984f1e579359a73044b6c48c014bdac32
joaquinrapetti <j.rapetti92@gmail.com>
2022-09-22 19:02:36 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Joaquin Rapetti Pozzoli on 22/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
